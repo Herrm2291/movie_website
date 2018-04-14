1335ce0f05266427a756a336cc6469b35702ad3a
Steve Herrmann <37193114+Herrm2291@users.noreply.github.com>
2018-04-14 17:57:24 -0400

    added fresh_tomatoes.py and a total of 6 movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,4 +10,22 @@ avatar = media.Movie("Avatar",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-avatar.show_trailer()
+school_of_rock = media.Movie("School of Rock",
+                             "Struggling rock singer disguises as a substitute teacher at a prep school",
+                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+
+men_in_black = media.Movie("Men in Black",
+                             "2 MIB agents try to save the world from aliens",
+                             "https://upload.wikimedia.org/wikipedia/en/f/fb/Men_in_Black_Poster.jpg",
+                             "https://www.youtube.com/watch?v=HYUd7AOw_lk")
+
+the_little_mermaid = media.Movie("The Little Mermaid",
+                             "A mermaid wants to be human",
+                             "https://upload.wikimedia.org/wikipedia/en/7/75/Movie_poster_the_little_mermaid.jpg",
+                             "https://www.youtube.com/watch?v=ZGZX5-PAwR8")
+
+pulp_fiction = media.Movie("Pulp Fiction",
+                             "2 hitmen try to recover a briefcase for their boss",
+                             "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
+                             "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
